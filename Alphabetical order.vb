3a6d7c50d3f2c8141a92b3a5de0d559fcd7ce06a
MustafaAli564 <55464035+MustafaAli564@users.noreply.github.com>
2019-12-12 22:20:54 +0500

    Add files via upload

---
+++ Alphabetical order.vb
@@ -0,0 +1,48 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myarr(500), x, y, bub As String
+        Dim i, j, no As Integer
+        Dim nextchar As Char
+
+
+        i = 0
+        nextchar = ""
+        x = ""
+        y = ""
+        j = 0
+        bub = ""
+        no = 0
+
+        Console.Write("How many Names/Words would you like to arrange?: ")
+        no = Console.ReadLine
+
+        For i = 1 To no
+            myarr(i) = ""
+        Next
+
+        For i = 1 To no
+            Console.Write("Enter a name or word: ")
+            x = Console.ReadLine
+            y = UCase(x)
+            myarr(i) = y
+        Next
+        For i = no - 1 To 1 Step -1
+            For j = 1 To i
+                If myarr(j) > myarr(j + 1) Then
+                    bub = myarr(j)
+                    myarr(j) = myarr(j + 1)
+                    myarr(j + 1) = bub
+                End If
+            Next
+        Next
+
+        For i = 1 To no
+            Console.WriteLine(myarr(i))
+        Next
+
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Character Counter(WITH ARRAYS).vb
@@ -0,0 +1,58 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myarr(26), i, x, y As Integer
+        Dim str1 As String
+        Dim nextchar, C As Char
+        Dim valid As Boolean
+
+        i = 0
+        str1 = ""
+        nextchar = ""
+        x = 0
+        C = ""
+        valid = False
+
+        For i = 1 To 26
+            myarr(i) = 0
+        Next
+
+        Console.Write("Enter a phrase: ")
+        str1 = Console.ReadLine
+        str1 = LCase(str1)
+        str1 = Replace(str1, " ", "")
+
+        For i = 1 To Len(str1)
+            nextchar = Mid(str1, i, 1)
+            x = Asc(nextchar) - 96
+            myarr(x) = myarr(x) + 1
+        Next
+
+
+        For i = 1 To 26
+            If myarr(i) > 1 Then
+                valid = True
+                Exit For
+            End If
+        Next
+        If valid = True Then
+            Console.WriteLine("-->THE REPEATED CHARACTERS ARE<--")
+            For i = 1 To 26
+                If myarr(i) > 1 Then
+                    x = i + 96
+                    C = Chr(x)
+                    C = UCase(C)
+                    Console.WriteLine("            " & C & " = " & myarr(i))
+                End If
+            Next
+
+        End If
+        If valid = False Then
+            Console.Write("No repeated characters found")
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
