60bc24dd962a7f4773f76576435878550e2dd042
MustafaAli564 <55464035+MustafaAli564@users.noreply.github.com>
2021-03-18 22:30:52 +0500

    Create pangram.vb

---
+++ pangram.vb
@@ -0,0 +1,37 @@
+Module Module1
+
+    Sub Main()
+        Dim alpha, str1, x As String
+        Dim counter As Integer
+        Dim is_pangram As Boolean
+
+        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+        str1 = ""
+        counter = 0
+        is_pangram = True
+
+        Console.Write("Enter String To Check: ")
+        str1 = Console.ReadLine
+        str1 = UCase(str1)
+
+        If Len(str1) < 26 Then
+            is_pangram = False
+        Else
+            For counter = 1 To 26
+                If InStr(str1, Mid(alpha, counter, 1)) = 0 Then
+                    is_pangram = False
+                End If
+                If is_pangram = False Then
+                    Exit For
+                End If
+            Next
+
+
+        End If
+        If is_pangram = True Then
+            Console.Write("string is a pangram")
+        Else
+            Console.Write("string is not a pangram")
+        End If
+        Console.ReadKey()
+    End Sub
